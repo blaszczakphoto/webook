9dd547c718dcb92f72bba986215d8b983d57cba3
Mariusz <mariusz@mariusz-VirtualBox.(none)>
2013-11-06 11:06:45 +0100

    New project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Webook::Application.config.secret_token = '61819d67876af8de40d391810e5dfb065818a2cb00ef7a58af39a92f5549a6ef20586528547f24bc4e9f703840aaa62969518de828dae4c88d8e11d81438d712'
