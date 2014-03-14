6f126e5c3d1f4525e9f3ca8ba72e9b03231fe57c
Mariusz <mariusz.blaszczak@gmail.com>
2014-03-14 10:07:48 +0100

    fix utf insequence error

--- lib/content_cleaner.rb
+++ lib/content_cleaner.rb
@@ -10,9 +10,13 @@ class ContentCleaner
 	end
 
 	def self.force_utf(content)
-		ic = Iconv.new('UTF-8', 'UTF-8//IGNORE')
+		# ic = Iconv.new('UTF-8', 'UTF-8//IGNORE')
+		ic = Iconv.new('UTF-8//IGNORE', 'UTF-8')
+		
 		begin
+			# debugger
 			content = ic.iconv(content + ' ')[0..-2]
+			# content = ic.iconv(content)
 		rescue
 			return ""
 		else
