f38d38d30c3b2440074394f6a2f4c3591577183b
Mariusz <mariusz.blaszczak@gmail.com>
2014-05-06 13:38:51 +0200

    fix if can not open image

--- lib/image_downloader.rb
+++ lib/image_downloader.rb
@@ -10,14 +10,18 @@ class ImageDownloader
 	end
 
 	# Process images of html, store locally and return html with changed paths
-	# 
 	def download
 		@images_dir = "#{@book_dir}/images"
 		FileUtils.makedirs(@images_dir) unless File.exists?(@images_dir)
 		doc = Nokogiri::HTML(@html)
 
 		doc.search("img").each do |img|
-			img.attribute("src").value = store_image(img, new_path(img))
+			new_path = store_image(img, new_path(img))
+			if new_path
+				img.attribute("src").value = new_path
+			else
+				img.remove
+			end
 		end
 		doc.to_html
 	end
@@ -33,14 +37,16 @@ class ImageDownloader
 	# Open and store image
 	def store_image(img, new_path)
 		begin
-		open(prepare_src(img.attribute("src").value)) do |f|
-			File.open(new_path,"wb") do |file|
-				file.puts f.read
+			url = prepare_src(img.attribute("src").value)
+			response = open(url)
+			if response.content_type.include?("image")
+				File.open(new_path, 'wb') {|f| f.write(response.read)}
+			else
+				new_path = false
 			end
+		rescue
+			new_path = false
 		end
-	rescue
-		debugger
-	end
 		return new_path
 	end
 
